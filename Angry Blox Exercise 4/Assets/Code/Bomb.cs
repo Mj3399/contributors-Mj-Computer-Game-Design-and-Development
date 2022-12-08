9f73fb88c3097ef0fda108982804f3fe920c1c0c
Mj3399 <114370635+Mj3399@users.noreply.github.com>
2022-12-07 21:26:11 -0600

    added Exercise 4

---
+++ Angry Blox Exercise 4/Assets/Code/Bomb.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    public float ThresholdForce = 5000;
+    public GameObject ExplosionPrefab;
+
+    private void Destruct()
+    {
+        Destroy(gameObject);
+    }
+    private void Boom()
+    {
+        GetComponent<PointEffector2D>().enabled = true;
+        GetComponent<SpriteRenderer>().enabled = false;
+        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+       
+        Invoke("Destruct", 0.1f);
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+
+    {
+
+        //Debug.Log(coll.collider);
+        if(null != coll.collider.GetComponent<Rigidbody2D>())
+        {
+            var vel = coll.collider.GetComponent<Rigidbody2D>().velocity.magnitude;
+                if (vel >= ThresholdForce)
+            {
+                Boom();
+
+            }
+        }
+
+        
+    }
+
+
+}
---
+++ Angry Blox Exercise 4/Assets/Code/TargetBox.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    /// 
+    public Rigidbody2D rb;
+
+    public static float OffScreen;
+    public GameObject Ground;
+
+    SpriteRenderer m_SpriteRenderer;
+    
+   
+        //
+    internal void Start() {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+        
+            Scored();
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+  
+    {
+
+        //Debug.Log(coll.collider);
+
+        if (GameObject.FindWithTag("Ground") == coll.collider.gameObject)
+        {
+            Scored();
+            
+        }
+    }
+    private void Scored()
+    {
+
+
+        if (m_SpriteRenderer.color != Color.green)
+        {
+            ScoreKeeper.AddToScore(rb.mass);
+        }
+        m_SpriteRenderer.color = Color.green;
+        
+        
+    }
+}
